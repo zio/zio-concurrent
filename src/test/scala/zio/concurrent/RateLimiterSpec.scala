e731436c911bfc60ee7a83eda4d0b70febd7e2e5
Dmitry Karlinsky <dmitryka@wix.com>
2020-03-04 10:29:57 +0200

    RateLimiterSpec: getting rid of sleeps in favor of waiting for counter to advance

--- src/test/scala/zio/concurrent/RateLimiterSpec.scala
+++ src/test/scala/zio/concurrent/RateLimiterSpec.scala
@@ -1,33 +1,37 @@
 package zio.concurrent
 
+import zio.ZIO
 import zio.concurrent.RateLimiter.Frequency
-import zio.{ clock, Ref, ZIO }
+import zio.concurrent.TestUtils.AwaitableRef
 import zio.test._
-import zio.test.environment.{ Live, TestClock }
-import zio.duration._
+import zio.test.environment.TestClock
 
 object RateLimiterSpec
     extends DefaultRunnableSpec(
       suite("RateLimiter") {
         testM("should limit rate") {
-          val buffer = 10
+          val buffer    = 1
+          val frequency = Frequency.unsafePerSecond(1.0)
+          val periods   = 3
+          def adjustClockAndWait(times: Int, counter: AwaitableRef[Int]) =
+            ZIO.foreach_(1 to times) { _ =>
+              counter.doAndAwaitChange(TestClock.adjust(frequency.period))
+            }
           for {
-            counter <- Ref.make(0)
-            limiter <- RateLimiter.make(Frequency.unsafePerSecond(1.0), buffer = buffer)
-
+            counter <- AwaitableRef.make(0)
+            limiter <- RateLimiter.make(frequency, buffer)
             producerFiber <- ZIO
                               .foreach(1 to 1000) { _ =>
                                 limiter.rateLimit(counter.update(_ + 1))
                               }
                               .fork
-            _ <- TestClock.adjust(1.second)
-            // give async stuff some time to work
-            _ <- Live.live(clock.sleep(100.millis))
+            _ <- counter.await(_ == buffer + 1)
+            _ <- adjustClockAndWait(periods, counter)
             // stop the producer fiber
             _            <- producerFiber.interrupt
             counterAfter <- counter.get
           } yield {
-            assert(counterAfter, Assertion.equalTo(buffer + 2))
+            assert(counterAfter, Assertion.equalTo(buffer + periods + 1))
           }
         }
       }
